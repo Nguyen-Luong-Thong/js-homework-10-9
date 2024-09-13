d9d5643682abefaa63b2ee83f6936d7377cf10b5
Nguyen-Luong-Thong <thong.nguyen@26student.passerellesnumeriques.org>
2024-09-13 14:18:09 +0700

    I coded for bai3

---
+++ bai3/bai3.js
@@ -0,0 +1,20 @@
+var id=0;
+total=0
+function myFunction1(){
+    event.preventDefault();
+    id++;
+    var a = document.getElementById('na').value;
+    var b = parseInt(document.getElementById('qu').value);
+    var c = parseInt(document.getElementById('pr').value);
+    var d = b*c;
+    total +=d;
+    var row= "<tr>";
+    row += "<td>"+id+"</td>";
+    row += "<td>"+a+"</td>";
+    row += "<td>"+b+"</td>";
+    row += "<td>"+c+"</td>";
+    row += "<td>"+d+"</td>";
+    row += "</tr>";
+    document.getElementById('tbody').innerHTML +=row;
+    document.getElementById('total').innerHTML =total;
+}
